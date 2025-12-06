a318cd81a26898b06c91f8cf48fdca9a95244022
Jean <oka.jeanjaures@gmail.com>
2025-12-06 15:31:37 +0100

    booking list

--- src/app/_core/services/booking-service.ts
+++ src/app/_core/services/booking-service.ts
@@ -6,14 +6,14 @@ import {ReservationType} from '../../_features/shared/booking/booking-list-compo
 import { BookingView} from '../dto/booking';
 import {AuthService} from './auth-service';
 import {environment} from '../../../environments/environment';
-import {ReservationFilter} from '../dto/reservation';
+import {BookingFilter} from '../dto/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
 
-  reservationFilter: ReservationFilter = new ReservationFilter() ;
+  filter: BookingFilter = new BookingFilter() ;
   private readonly baseUrl = `${environment.apiUrl}`;
   private readonly bookingEndpoint =`${this.baseUrl}/api/booking`;
 
@@ -21,13 +21,34 @@ export class BookingService {
               private authService: AuthService) {}
 
 
-  loadReservations(filter?: ReservationFilter ): Observable<ResponseWrapper<BookingView[]>> {
+  private formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  }
+
+
+  loadReservations(filter?: BookingFilter ): Observable<ResponseWrapper<BookingView[]>> {
 
     let params = new HttpParams();
     if (filter) {
       Object.entries(filter).forEach(([key, value]) => {
-        if (value ! == undefined && value !== null && value !== '') {
-          params = params.set(key, value.toString());
+        if (value !== undefined && value !== null && value !== '') {
+
+          if (key === 'startDate' && value instanceof Date) {
+            params = params.set(key, this.formatDate(value));
+          } else if (key === 'endDate' && value instanceof Date) {
+            params = params.set(key, this.formatDate(value));
+          }
+          else if (Array.isArray(value)) {
+            value.forEach(v => {
+              params = params.append(key, v);
+            });
+          }
+          else {
+            params = params.set(key, value.toString());
+          }
         }
       });
     }
@@ -49,5 +70,13 @@ export class BookingService {
     return this.http.get<ReservationType[]>('assets/data/reservation-types.json');
   }
 
+
+  clearFilter(field: keyof BookingFilter){
+    if(Array.isArray(this.filter[field])) {
+      (this.filter[field] as any[]) = [];
+    } else{
+      this.filter[field] = undefined;
+    }
+  }
 
 }
--- src/app/_features/shared/booking/booking-list-component/booking-list-component.ts
+++ src/app/_features/shared/booking/booking-list-component/booking-list-component.ts
@@ -2,11 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {BookingService} from '../../../../_core/services/booking-service';
 import {DatePicker} from 'primeng/datepicker';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {InputNumber} from 'primeng/inputnumber';
 import {InputText} from 'primeng/inputtext';
 import {MultiSelect} from 'primeng/multiselect';
 import {NavbarTop} from '../../../../_core/layout/navbar-top/navbar-top';
-import {Button} from 'primeng/button';
 import {Tag} from 'primeng/tag';
 import {CommonModule, DatePipe} from '@angular/common';
 import {TableModule} from 'primeng/table';
@@ -14,8 +12,9 @@ import {Loader} from '../../../../_core/layout/loader/loader';
 import {AlertService} from '../../../../_core/services/alert-service';
 import { BookingView} from '../../../../_core/dto/booking';
 import {ResponseWrapper} from '../../../../_core/dto/responseWrapper';
-import {Tooltip} from 'primeng/tooltip';
 import {AsideMenuComponent} from '../../../../_core/layout/aside-menu-component/aside-menu-component';
+import {Chip} from 'primeng/chip';
+import {BookingFilter} from '../../../../_core/dto/reservation';
 
 
 
@@ -35,13 +34,11 @@ export interface ReservationType {
     MultiSelect,
     NavbarTop,
     ReactiveFormsModule,
-    Button,
-    Tag,
-    DatePipe,
     TableModule,
     Loader,
-    Tooltip,
-    AsideMenuComponent
+    AsideMenuComponent,
+    Tag,
+    Chip
   ],
   templateUrl: './booking-list-component.html',
   standalone: true,
@@ -54,11 +51,14 @@ export class BookingListComponent implements OnInit{
   filteredReservations: BookingView[] =[] ;
   typeParams: ReservationType[] =[] ;
 
+  activeFilters: { key: keyof BookingFilter; label: string; value: string }[] = [];
+
   constructor(protected bookingService: BookingService,
               protected alertService: AlertService) {}
 
   ngOnInit(): void {
 
+    this.resetFilters();
     this.loadBookingType()
     this.loadReservation()
   }
@@ -70,11 +70,13 @@ export class BookingListComponent implements OnInit{
 
   loadReservation() {
     this.isLoading = true;
-    this.bookingService.loadReservations()
+    this.filteredReservations = []
+    this.bookingService.loadReservations(this.bookingService.filter)
       .subscribe({
         next: (res: ResponseWrapper<BookingView[]>) => {
           this.filteredReservations = res.data;
           this.isLoading = false;
+          this.updateActiveFilters()
         },
         error: (err: any) => {
           console.log(`Error http when fetching bookings : ${err}`);
@@ -100,5 +102,99 @@ export class BookingListComponent implements OnInit{
 
   delete(res: any) {
 
+  }
+
+
+
+  updateActiveFilters() {
+    const f = this.bookingService.filter;
+    this.activeFilters = [];
+
+    if (f.name && f.name.trim() !== '') {
+      this.activeFilters.push({ key: 'name', label: 'Utilisateur', value: f.name });
+    }
+
+    if (f.type && f.type.trim() !== '') {
+      const typeLabel = this.typeParams.find(t => t.value === f.type)?.label || f.type;
+      this.activeFilters.push({ key: 'type', label: 'Type', value: typeLabel });
+    }
+
+    if (f.startDate) {
+      this.activeFilters.push({
+        key: 'startDate',
+        label: 'Date début',
+        value: new Date(f.startDate).toLocaleDateString('fr-FR')
+      });
+    }
+
+    if (f.endDate) {
+      this.activeFilters.push({
+        key: 'endDate',
+        label: 'Date fin',
+        value: new Date(f.endDate).toLocaleDateString('fr-FR')
+      });
+    }
+  }
+  /*updateActiveFilters() {
+    const f = this.bookingService.filter
+    this.activeFilters = [];
+
+    if (f.id) {
+      this.activeFilters.push({ key: 'id', label: 'Id Réservation', value: f.id });
+    }
+
+    if (f.name) {
+      this.activeFilters.push({ key: 'name', label: 'Utilisateur', value: f.name });
+    }
+
+    if (f.type) {
+      const typeLabel = this.typeParams.find(t => t.value === f.type)?.label || f.type;
+      this.activeFilters.push({ key: 'type', label: 'Type', value: typeLabel });
+    }
+
+    if (f.startDate) {
+      this.activeFilters.push({
+        key: 'startDate',
+        label: 'Date début',
+        value: new Date(f.startDate).toLocaleDateString('fr-FR')
+      });
+    }
+
+    if (f.endDate) {
+      this.activeFilters.push({
+        key: 'endDate',
+        label: 'Date fin',
+        value: new Date(f.endDate).toLocaleDateString('fr-FR')
+      });
+    }
+
+  }*/
+
+  resetFilters(): void {
+    this.bookingService.filter = {
+      name: '',
+      type: '',
+      startDate: undefined,
+      endDate: undefined
+    };
+    this.activeFilters = [];
+  }
+
+  removeFilter(filterKey: keyof BookingFilter){
+    this.bookingService.clearFilter(filterKey);
+    this.loadReservation()
+  }
+
+
+  searchBookings(): void {
+    const filter = this.bookingService.filter;
+    if (filter.startDate && filter.endDate) {
+      if (filter.startDate > filter.endDate) {
+        this.alertService.warn("La date de début ne peut pas être supérieure à la date de fin.");
+        return;
+      }
+    }
+
+    this.loadReservation();
   }
 }
